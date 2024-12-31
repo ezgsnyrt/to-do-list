b5d7007c17772e237a53c6f527f5b492e092d907
ezgsnyrt <ezgi.senyurt3@gmail.com>
2024-12-31 03:23:49 +0100

    Added Mongoose taskSchema
    Defined Task model for MongoDB collection
    
    Enabled automatic management of createdAt and updatedAt fields using timestamps

--- server/index.js
+++ server/index.js
@@ -13,6 +13,34 @@ mongoose
         console.error("Failed to connect to MongoDB", err);
     });
 
+    const taskSchema = new mongoose.Schema({
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: false,
+        },
+        status: {
+            type: String,
+            enum: ["pending", "completed"],
+            default: "pending",
+        },
+        priority: {
+            type: String,
+            enum: ["low", "medium", "high"],
+            default: "medium",
+        },
+        dueDate: {
+            type: Date,
+        },
+    },
+    { timestamps: true }, // Update createdAt and updatedAt automatically
+);
+
+const Task = mongoose.model("task", taskSchema);
+
 app.listen(3000, () =>
     console.log("Server is running at http://localhost:3000")
 );
